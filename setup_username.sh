3f1313d35858851445b1613307600fea0dc649a2
Solvers TheProblem <134174564+buildkitebuckley@users.noreply.github.com>
2023-05-23 23:21:50 -0400

    maybe we have shadow hello overshadowing

--- setup_username.sh
+++ setup_username.sh
@@ -2,9 +2,9 @@
 
 HELLO_NAME=$(buildkite-agent meta-data get hello-name)
 
-buildkite-agent artifact download hello . --step k8s-image-build
+buildkite-agent artifact download hello ./compiled_hello --step k8s-image-build
 
 echo "We would say hi to ${HELLO_NAME}"
-ls -laRt hello
-chmod +x hello/hello
-./hello/hello ${HELLO_NAME}
+ls -laRt .
+chmod +x compiled_hello
+./compiled_hello ${HELLO_NAME}
