ba216b3557850913a0be52eb1b130f176fcac1e2
Solvers TheProblem <134174564+buildkitebuckley@users.noreply.github.com>
2023-05-23 23:27:48 -0400

    cleanup some of my debug printfs

--- setup_username.sh
+++ setup_username.sh
@@ -5,7 +5,5 @@ HELLO_NAME=$(buildkite-agent meta-data get hello-name)
 mkdir ./myhello
 buildkite-agent artifact download hello ./myhello/ --step k8s-image-build
 
-echo "We would say hi to ${HELLO_NAME}"
-ls -laRt .
 chmod +x ./myhello/hello
 ./myhello/hello ${HELLO_NAME}
