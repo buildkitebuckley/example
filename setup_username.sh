92cc978143ed2e9f9d40310076ccba17e5e6c6b8
Solvers TheProblem <134174564+buildkitebuckley@users.noreply.github.com>
2023-05-23 23:25:18 -0400

    so no renaming, but perhaps alternate path?

--- setup_username.sh
+++ setup_username.sh
@@ -2,9 +2,10 @@
 
 HELLO_NAME=$(buildkite-agent meta-data get hello-name)
 
-buildkite-agent artifact download hello ./compiled_hello --step k8s-image-build
+mkdir ./myhello
+buildkite-agent artifact download hello ./myhello/ --step k8s-image-build
 
 echo "We would say hi to ${HELLO_NAME}"
 ls -laRt .
-chmod +x compiled_hello
-./compiled_hello ${HELLO_NAME}
+chmod +x ./myhello/hello
+./myhello/hello ${HELLO_NAME}
