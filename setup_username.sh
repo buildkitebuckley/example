f03ab21bee4aa44dd2eeae1039790b421addc357
Solvers TheProblem <134174564+buildkitebuckley@users.noreply.github.com>
2023-05-23 23:11:29 -0400

    artifact download not sure of step name

--- setup_username.sh
+++ setup_username.sh
@@ -2,4 +2,8 @@
 
 HELLO_NAME=$(buildkite-agent meta-data get hello-name)
 
+buildkite-agent artifact download hello . --step k8s-image-build
+
 echo "We would say hi to ${HELLO_NAME}"
+chmod +x hello
+hello ${HELLO_NAME}"
