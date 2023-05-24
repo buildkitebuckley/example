17ac62d625356e15650b49508133e34f19932ca7
Solvers TheProblem <134174564+buildkitebuckley@users.noreply.github.com>
2023-05-23 23:18:38 -0400

    hello is still a mysterious directory

--- setup_username.sh
+++ setup_username.sh
@@ -5,5 +5,6 @@ HELLO_NAME=$(buildkite-agent meta-data get hello-name)
 buildkite-agent artifact download hello . --step k8s-image-build
 
 echo "We would say hi to ${HELLO_NAME}"
-chmod +x hello
-./hello ${HELLO_NAME}
+ls -laRt hello
+chmod +x hello/hello
+./hello/hello ${HELLO_NAME}
